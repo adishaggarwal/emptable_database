c9389da4a1fb4c6a404c1504ef20b7130b01293c
adish aggarwal <aggarwaladish@gmail.com>
2020-04-19 00:21:14 +0530

    jl

--- api.js
+++ api.js
@@ -26,11 +26,11 @@ app.delete("/deleterow/:id", function (req, res) {
     });
 });
 app.put("updateuser/:id", function (req, res) {
+    console.log("hello");
     let id = req.params.id;
     client.query(`update users set firstname=$1,middlename=$2,lastname=$3,email=$4,phoneno=$5,address=$6,role=$7,customer_id=$8
   where empid=${id};`, [value.firstname, value.middlename, value.lastname, value.email, value.phoneno, value.address, value.role_name, value.customer_id]);
     res.send();
-    console.log("hello");
 });
 app.get('/getroles', function (req, res) {
     client.query(`select role_name,role_key from roles`, (error, result) => {
