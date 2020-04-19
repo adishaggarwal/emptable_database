148d9a72d4b612181a14424e04f5cab0d75f41fc
adish aggarwal <aggarwaladish@gmail.com>
2020-04-20 03:14:55 +0530

    final project

--- app1.js
+++ app1.js
@@ -46,64 +46,13 @@ class actions {
             };
         }
     }
-    addrow() {
-    }
     editrow(val) {
         var fid;
         var yo;
         var kid = 1;
         var lol = 7;
-        /*
-        if (val == "newsave" || val=="newcancel") {
-            let theid = document.getElementById("theid")!;
-            let theval = theid.value;
-            if(theval)
-            {
-                let therow = document.getElementById("newnew")!;
-                therow.id = "row" + theval;
-               
-                let thesave = document.getElementById("newsave")!;
-                thesave.id = "" + theval + "" + theval;
-
-                let thedelete = document.getElementById("newcancel")!;
-                thedelete.innerHTML = "DELETE";
-                thedelete.id = "" + theval;
-
-                fid = "" + theval;
-                yo = "" + theval + "" + theval;
-                lol=99;
-
-                let dnew = document.getElementById(""+fid)!;
-                dnew.onclick = () => {
-                    p1.deleterow(fid);
-                };
-                let pnew = document.getElementById(""+yo)!;
-                pnew.onclick = () => {
-                    p1.editrow(fid);
-                   };
-        }
-            else
-            {
-                let rowid = document.getElementById("newnew")!;
-                let theid = document.getElementById("theid")!;
-                let thesave = document.getElementById("newsave")!;
-                let thecancel = document.getElementById("newcancel")!;
-                theid.removeAttribute("id");
-                thesave.removeAttribute("id");
-                thecancel.removeAttribute("id");
-                rowid.id="fake";
-                document.getElementById("fake")!.remove();
-                fid = "nosorry";
-                kid = 99;
-                p1.nrow=1;
-            }
-           
-        }
-        */
-        {
-            fid = "" + val;
-            yo = "" + val + "" + val;
-        }
+        fid = "" + val;
+        yo = "" + val + "" + val;
         if (kid == 1) {
             if (document.getElementById("" + fid + "" + fid).innerHTML == "SAVE") {
                 let recordarr;
@@ -111,19 +60,23 @@ class actions {
                 let b6 = "row" + fid;
                 var xx6 = document.getElementById(b6).getElementsByTagName("input");
                 var xe6 = document.getElementById(b6).getElementsByTagName("td");
-                var xs61 = document.getElementById("selectroles" + fid);
-                var xs62 = document.getElementById("selectwebsites" + fid);
+                //var xs61= document.getElementById("selectroles"+fid)!;
+                //var xs62= document.getElementById("selectwebsites"+fid)!;
+                let sel6 = document.getElementById("selectroles" + fid);
+                let sel7 = document.getElementById("selectwebsites" + fid);
+                let text6 = sel6.options[sel6.selectedIndex].value;
+                let text7 = sel7.options[sel7.selectedIndex].value;
                 var q6;
                 for (q6 = 0; q6 < xe6.length - 2; q6++) {
                     var index = xx6[q6];
                     if (q6 == 6) {
                         let x;
-                        x = xs62[0].value;
+                        x = text7;
                         recordarr[q6] = x;
                     }
                     else if (q6 == 7) {
                         let x;
-                        x = xs61[0].value;
+                        x = text6;
                         recordarr[q6] = x;
                     }
                     else {
@@ -151,7 +104,6 @@ class actions {
                 if (validate.phone(`${recordarr[4]}`) && validate.email(`${recordarr[3]}`) && validate.notempty(`${recordarr[0]}`) && validate.notempty(`${recordarr[2]}`) && validate.address(`${recordarr[5]}`)) {
                     let newemp = new imp.emp(recordarr[0], recordarr[1], recordarr[2], recordarr[3], recordarr[4], recordarr[5], recordarr[6], recordarr[7]);
                     let alerts = document.getElementsByClassName("alertspan");
-                    console.log(newemp);
                     for (let i = 0; i < alerts.length; i++) {
                         alerts[i].style.display = "none";
                     }
@@ -206,15 +158,11 @@ class actions {
                      }*/
                     //  {
                     console.log(newemp);
-                    fetch(`http://localhost:3000/updateuserdata/${fid}`, {
+                    fetch(`http://localhost:3000/updateUser/${fid}`, {
                         method: "put",
-                        headers: { 'Content-Type': 'application/json',
-                            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
-                            'Access-Control-Allow-Origin': '*',
-                            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization' },
+                        headers: { 'Content-Type': 'application/json' },
                         body: JSON.stringify(newemp)
                     }).then(res => {
-                        console.log("ebkj0");
                         let kalesh1 = document.getElementById("tbody").getElementsByTagName("button");
                         for (let i = 1; i < kalesh1.length; i = i + 2) {
                             let num2 = kalesh1[i].id;
@@ -232,8 +180,8 @@ class actions {
                                 let sel7 = document.getElementById("selectwebsites" + fid);
                                 let text6 = sel6.options[sel6.selectedIndex].text;
                                 let text7 = sel7.options[sel7.selectedIndex].text;
-                                sel[6].innerHTML = `<input type="number" placeholder="${text6}" disabled>`;
-                                sel[7].innerHTML = `<input type="number" placeholder="${text7}" disabled>`;
+                                sel[6].innerHTML = `<input type="number" placeholder="${text7}" disabled>`;
+                                sel[7].innerHTML = `<input type="number" placeholder="${text6}" disabled>`;
                                 var q5;
                                 for (q5 = 0; q5 < xx5.length; q5++) {
                                     var index = xx5[q5];
@@ -325,29 +273,7 @@ class actions {
     deleterow(val) {
         var num;
         var kya = 1;
-        /*   if(val=="newcancel")
-           {
-               if(document.getElementById("theid")!.value){
-            let theid = document.getElementById("theid")!;
-                let theval = theid.value;
-                let therow = document.getElementById("newnew")!;
-                therow.id="row" + theval;
-                let thesave = document.getElementById("newsave")!;
-                thesave.innerHTML = "EDIT";
-                thesave.id="" + theval + "" + theval;
-                let thedelete = document.getElementById("newcancel")!;
-                thedelete.innerHTML = "DELETE";
-                thedelete.id="" + theval;
-                num = "" + theval;
-               }
-               else{
-                   p1.editrow("newcancel");
-                   kya=99;
-               }
-           }*/
-        {
-            num = "" + val;
-        }
+        num = "" + val;
         if (kya == 1) {
             if ((document.getElementById("" + num).innerHTML == "DELETE")) {
                 fetch(`http://localhost:3000/deleterow/${num}`, {
@@ -360,54 +286,124 @@ class actions {
                     thedel.removeAttribute("id");
                     rowid.id = "fake";
                     document.getElementById("fake").remove();
+                    console.log(num);
                 });
             }
-            /*
-                else
-                {
-                    let h2= "row"+ num;
-                    let nh1=num;
-                               
-                                var h22= document.getElementById(h2)!;
-        
-                                h22.innerHTML=`
+            else {
+                let h2 = "row" + num;
+                let nh1 = num;
+                var h22 = document.getElementById(h2);
+                h22.innerHTML = `
                                 <td><input type="text" placeholder="${p1.temp[0]}" disabled></td>
                                 <td><input type="text" placeholder="${p1.temp[1]}" disabled></td>
                                 <td><input type="text" placeholder="${p1.temp[2]}" disabled></td>
                                 <td><input type="email" placeholder="${p1.temp[3]}" disabled></td>
                                 <td><input type="number" placeholder="${p1.temp[4]}" disabled></td>
-                                <td><input type="number" placeholder="${p1.temp[5]}" disabled></td>
-                                <td><input type="text" placeholder="${p1.temp[6]}" disabled></td>
+                                <td><input type="text" placeholder="${p1.temp[5]}" disabled></td>
+                                <td><input type="number" placeholder="${p1.temp[6]}" disabled></td>
                                 <td><input type="number" placeholder="${p1.temp[7]}" disabled></td>
                                 <td><button id="${nh1}${nh1}" class="btn btn-primary">EDIT</button></td>
                                 <td><button id="${nh1}" class="btn btn-danger">DELETE</button></td>`;
-        
-                                
-                                let r=""+ num + "" + num;
-                                let de = document.getElementById(""+nh1)!;
-                                de!.onclick = () => {
-                                    this.deleterow(p1.temp[6]);
-                                  }
-                         
-                         let pe = document.getElementById(r)!;
-                         pe!.onclick = () => {
-                            this.editrow(this.temp[6]);
-                          }
-                    let kalesh3=document.getElementById("tbody")!.getElementsByTagName("button")!;
-        
-                    for(let i=1;i<kalesh3.length;i=i+2)
-                    {
-                        let num2=kalesh3[i].id;
-                        let b25=""+num2;
-                        let b35=""+num2+ "" +num2;
-                        var xx15= document.getElementById(""+b25)!;
-                        var xx25= document.getElementById(""+b35)!;
-                            xx15.style.display="block";
-                            xx25.style.display="block";
-                    }
+                let r = "" + num + "" + num;
+                let de = document.getElementById("" + nh1);
+                de.onclick = () => {
+                    this.deleterow(num);
+                };
+                let pe = document.getElementById(r);
+                pe.onclick = () => {
+                    this.editrow(num);
+                };
+                let kalesh3 = document.getElementById("tbody").getElementsByTagName("button");
+                for (let i = 1; i < kalesh3.length; i = i + 2) {
+                    let num2 = kalesh3[i].id;
+                    let b25 = "" + num2;
+                    let b35 = "" + num2 + "" + num2;
+                    var xx15 = document.getElementById("" + b25);
+                    var xx25 = document.getElementById("" + b35);
+                    xx15.style.display = "block";
+                    xx25.style.display = "block";
                 }
-                */
+            }
         }
+    }
+    async addrow() {
+        await fetchdata.fetch().then(data => {
+            var len = data.length;
+            let ind = data[len - 1].empid + 1;
+            p1.newid = ind;
+        });
+        console.log(p1.newid);
+        let kalesh = await document.getElementById("tbody").getElementsByTagName("button");
+        let thebody = document.getElementById("tbody");
+        p1.nrow = 0;
+        let newemp = new imp.emp2("", "", "", "dummy@gmail.com", 9999999999, "", 1, 1, p1.newid);
+        let newlen = kalesh.length;
+        fetch(`http://localhost:3000/addnewrow`, {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newemp)
+        }).then((res) => res.json())
+            .then(res => {
+            let newrow = thebody.insertRow(-1);
+            newrow.setAttribute("id", "row" + p1.newid);
+            newrow.innerHTML = `
+       <td><input type="text" placeholder="firstname"></td>
+       <td><input type="text" placeholder="middlename"></td>
+       <td><input type="text" placeholder="lastname"></td>
+       <td><input type="email" placeholder="email"></td>
+       <td><input type="number" placeholder=""></td>
+       <td><input type="text" placeholder="address"></td>`;
+            newrow.innerHTML += ` <td><select id="selectwebsites${p1.newid}" class="form-control">
+       </select></td>
+         `;
+            newrow.innerHTML += `<td><select id="selectroles${p1.newid}" class="form-control">
+              </select></td>
+                `;
+            websitesrow.fetchwebsites().then(data => {
+                let select = document.getElementById("selectwebsites" + p1.newid);
+                for (let i = 0; i < data.length; i++) {
+                    let opt = data[i];
+                    let optelement = document.createElement("option");
+                    optelement.textContent = opt.website;
+                    let str = "" + opt.customer_id;
+                    optelement.value = str;
+                    select.appendChild(optelement);
+                }
+            });
+            rolesrow.fetchroles().then(data => {
+                let select = document.getElementById("selectroles" + p1.newid);
+                for (let i = 0; i < data.length; i++) {
+                    let opt = data[i];
+                    let optelement = document.createElement("option");
+                    optelement.textContent = opt.role_name;
+                    let str = "" + opt.role_key;
+                    optelement.value = str;
+                    select.appendChild(optelement);
+                }
+            });
+            newrow.innerHTML += `<td><button class="btn btn-primary" id="${p1.newid}${p1.newid}">SAVE</button></td>
+        <td><button class="btn btn-danger" id="${p1.newid}">DELETE</button></td>`;
+            let dnew = document.getElementById("" + p1.newid);
+            dnew.onclick = () => {
+                p1.deleterow(p1.newid);
+            };
+            let pnew = document.getElementById("" + p1.newid + "" + p1.newid);
+            pnew.onclick = () => {
+                p1.editrow(p1.newid);
+            };
+            for (let i = 1; i < newlen; i = i + 2) {
+                let a = "" + kalesh[i].id;
+                let b = "" + kalesh[i].id + "" + kalesh[i].id;
+                let d = document.getElementById("" + a);
+                d.onclick = () => {
+                    p1.deleterow(a);
+                };
+                let p = document.getElementById("" + b);
+                p.onclick = () => {
+                    p1.editrow(a);
+                };
+            }
+        });
     }
 }
 let p1 = new actions();
