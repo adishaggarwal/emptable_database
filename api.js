ec85f3fdc64931c7699f24dcf2d89b74997fd8b0
adish aggarwal <aggarwaladish@gmail.com>
2020-06-05 19:08:32 +0530

    image

--- api.js
+++ api.js
@@ -14,7 +14,7 @@ app.listen(3000);
 app.use(bodyParser.json());
 function getData(req, res) {
     client.connect().then(() => console.log("Successfully Connected"));
-    client.query('select users.empid,users.firstname,users.middlename,users.lastname,users.email,users.phoneno,roles.role_name,users.address,customer.website from users,customer,roles where users.role=roles.role_key AND users.customer_id=customer.customer_id order by users.empid asc;', (err, result) => {
+    client.query('select users.empid,users.firstname,users.middlename,users.lastname,users.email,users.phoneno,users.role,users.address,users.customer_id from users order by users.empid asc;', (err, result) => {
         res.status(200).json(result.rows);
     });
 }
@@ -29,8 +29,9 @@ app.put('/updateUser/:id', function (req, res) {
     let value = req.body;
     let id = req.params.id;
     client.query(`update users set firstname=$1,middlename=$2,lastname=$3,email=$4,phoneno=$5,address=$6,customer_id=$7,role=$8
-  where empid=${id};`, [value.firstname, value.middlename, value.lastname, value.email, value.phoneno, value.address, value.customer_id, value.role_name]);
+  where empid=${id};`, [value.firstname, value.middlename, value.lastname, value.email, value.phoneno, value.address, value.customer_id, value.role]);
     res.send();
+    console.log(value);
 });
 app.get('/getroles', function (req, res) {
     client.query(`select role_name,role_key from roles`, (error, result) => {
@@ -46,4 +47,35 @@ app.post('/addnewrow', function (req, res) {
     let value = req.body;
     client.query(`insert into users (firstname,middlename,lastname,email,phoneno,address,customer_id,role,empid) values($1,$2,$3,$4,$5,$6,$7,$8,$9) returning empid;`, [value.firstname, value.middlename, value.lastname, value.email, value.phoneno, value.address, value.customer_id, value.role_name, value.empid])
         .then((result) => res.json(result.rows[0]));
+});
+function getcustomer(req, res) {
+    client.connect().then(() => console.log("Successfully Connected"));
+    client.query('select * from customer order by customer_id asc;', (err, result) => {
+        res.status(200).json(result.rows);
+    });
+}
+app.get("/fetchcustomerdata", getcustomer);
+app.delete("/deletecustomer/:id", function (req, res) {
+    let id = req.params.id;
+    client.query(`delete from customer where customer_id=${id};`, (err, result) => {
+        res.send();
+    });
+});
+app.put('/updatecustomer/:id', function (req, res) {
+    let value1 = req.body;
+    let id = req.params.id;
+    client.query(`update customer set website='${value1.website}'
+  where customer_id=${id};`);
+    res.send();
+});
+app.post('/addnewcustomer', function (req, res) {
+    let value = req.body;
+    client.query(`insert into customer (customer_id,website) values($1,$2);`, [value.customer_id, value.website])
+        .then((result) => res.json(result.rows[0]));
+});
+app.get('/showusers/:id', function (req, res) {
+    let id = req.params.id;
+    client.query(`select firstname,middlename,lastname,email from users where customer_id=${id};`, (error, result) => {
+        res.status(200).json(result.rows);
+    });
 });
